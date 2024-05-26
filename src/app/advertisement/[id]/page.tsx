4ff2255b978b5480dd7e57cf48d9bb67474c4065
Mo. Saleh Gholami <iSalehGholami@gmail.com>
2024-05-26 17:54:36 +0330

    Updating old icons + css codes from Ad details

--- src/app/advertisement/[id]/page.tsx
+++ src/app/advertisement/[id]/page.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
 import styles from './style/ad_details.module.css';
-import { InfoCircle } from 'iconsax-react';
+import { ArchiveAdd, Fatrows, House2, InfoCircle, LampCharge, Link2, Messages3, NotificationCircle, RecordCircle, Ruler, UsdCoin } from 'iconsax-react';
 
 function page({ params }: any) {
     const id = params.id;
@@ -14,16 +14,6 @@ function page({ params }: any) {
                     <img src="/assets/images/plan.png" alt="" className={styles.planPic} />
                 </div>
                 <div className={styles.adDetailsWrapper}>
-                    <div className={styles.adVerificationTrustBox}>
-                        <div className={styles.adVerificationTrustText}>
-                            !همین حالا نشان اعتماد را به آگهی خود اضافه کنید
-                        </div>
-                        <div className={styles.adVerificationTrustButton}>
-                            <img className="ad-verification-trust-button-icon" src="/assets/icons/lamp-charge.svg" />
-                            <div className={styles.adVerificationTrustButtonText}>برای دریافت اطلاعات بیشتر کلیک کنید</div>
-                        </div>
-                    </div>
-
                     <div className={styles.adTitlesWrapper}>
                         <div className={styles.adTitle}>112 متر نوساز کلید نخورده ، ساری</div>
                         <div className={styles.adSubtitle}>نیم ساعت پیش در تهران ، سهروردی</div>
@@ -31,13 +21,13 @@ function page({ params }: any) {
 
                     <div className={styles.adActionBarContainer}>
                         <div className={styles.adShareWrapper}>
-                            <img src="assets/icons/link-2.svg" alt="" className={styles.adShareBtn} />
-                            <img src="assets/icons/archive-add.svg" alt="" className={styles.adShareBtn} />
+                            <Link2 className={styles.adShareBtn} />
+                            <ArchiveAdd className={styles.adShareBtn} />
                         </div>
                         <div className={styles.adCommunicationWrapper}>
                             <div className={`${styles.primaryButton} ${styles.btnChat}`}>
                                 <div className={styles.primaryBtnText}>چت</div>
-                                <img src="/assets/icons/messages-3.svg" alt="" className={styles.primaryBtnIcon} />
+                                <Messages3 className={styles.primaryBtnIcon} />
                             </div>
                             <div className={`${styles.primaryButton} ${styles.btnCallInfo}`}>
                                 <div className={styles.primaryBtnText}>اطلاعات تماس</div>
@@ -49,24 +39,24 @@ function page({ params }: any) {
                         <div className={styles.detailsListTile}>
                             <div className={styles.listTileBody}>24,000,000,000 تومان</div>
                             <div className={styles.listTileTitle}>قیمت کل :</div>
-                            <img src="/assets/icons/usd-coin-(usdc).svg" className="list-tile-icon" />
+                            <UsdCoin className={styles.listTileIcon} />
                         </div>
                         <div className={styles.detailsListTile}>
                             <div className={styles.listTileBody}>23,000,000 تومان</div>
                             <div className={styles.listTileTitle}>قیمت هر متر :</div>
-                            <img src="/assets/icons/ruler.svg" className="list-tile-icon" />
+                            <Ruler className={styles.listTileIcon} />
                         </div>
 
                         <div className={styles.detailsListTile}>
                             <div className={styles.listTileBody}>املاک ایوان</div>
                             <div className={styles.listTileTitle}>آژانس املاک :</div>
-                            <img src="/assets/icons/house-2.svg" className="list-tile-icon" />
+                            <House2 className={styles.listTileIcon} />
                         </div>
 
                         <div className={styles.detailsListTile}>
                             <div className={styles.listTileBody}>۳ از ۶</div>
                             <div className={styles.listTileTitle}>طبقه :</div>
-                            <img src="/assets/icons/fatrows.svg" className="list-tile-icon" />
+                            <Fatrows className={styles.listTileIcon} />
                         </div>
                     </div>
 
@@ -84,6 +74,15 @@ function page({ params }: any) {
                             </div>
                         </div>
                     </div>
+                    <div className={styles.adVerificationTrustBox}>
+                        <div className={styles.adVerificationTrustText}>
+                            !همین حالا نشان اعتماد را به آگهی خود اضافه کنید
+                        </div>
+                        <div className={styles.adVerificationTrustButton}>
+                            <LampCharge className={styles.adVerificationTrustButtonIcon} />
+                            <div className={styles.adVerificationTrustButtonText}>برای دریافت اطلاعات بیشتر کلیک کنید</div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className={styles.adBottomPiece}>
@@ -98,28 +97,31 @@ function page({ params }: any) {
                     <div className={styles.descriptionHeader}>توضیحات</div>
                     <div className={styles.descriptionBody}>
                         <div className={styles.descriptionItem}>
-                            <InfoCircle className="text-Blue-Primary" />
+                            <RecordCircle className="text-Blue-Primary" variant='Bold' />
+
                             <div className={styles.descriptionItemText}>
                                 3 خوابه 1 خواب مستر
                             </div>
                         </div>
 
                         <div className={styles.descriptionItem}>
-                            <InfoCircle className="text-Blue-Primary" />
+                            <RecordCircle className="text-Blue-Primary" variant='Bold' />
                             <div className={styles.descriptionItemText}>
                                 3 خوابه 1 خواب مستر
                             </div>
                         </div>
 
                         <div className={styles.descriptionItem}>
-                            <InfoCircle className="text-Blue-Primary" />
+                            <RecordCircle className="text-Blue-Primary" variant='Bold' />
+
                             <div className={styles.descriptionItemText}>
                                 3 خوابه 1 خواب مستر
                             </div>
                         </div>
 
                         <div className={styles.descriptionItem}>
-                            <InfoCircle className="text-Blue-Primary" />
+                            <RecordCircle className="text-Blue-Primary" variant='Bold' />
+
                             <div className={styles.descriptionItemText}>
                                 3 خوابه 1 خواب مستر
                             </div>
